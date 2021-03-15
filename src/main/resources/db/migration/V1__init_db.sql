ae2c2f8380c76c48a015dcc233a0dee79810633b
rustamov-bite <assasins_murza006@mail.ru>
2021-03-15 17:19:34 +0600

    added sql script init_db

---
+++ src/main/resources/db/migration/V1__init_db.sql
@@ -0,0 +1,7 @@
+insert into categories (name) values ("MacBook"), ("iPhone"), ("Apple Watch");
+
+insert into products (name, image, quantity, description, price, category_id) values
+("MacBook Pro Retina 13''", "images/macbookPro.jpg", 3, "Blah Blah Blah", 1000, 0),
+("MacBook Air 13", "images/macbookAir.jpg", 3, "Blah Blah Blah", 700, 0),
+("Iphone 8", "images/iphone8.jpg", 3, "Blah Blah Blah", 600, 1),
+("AppleWatch 3G", "images/appleWatch3.jpg", 3, "Blah Blah Blah", 1000, 2);
--- src/main/resources/db/migration/V2__FULL_DB.sql
+++
@@ -1,5 +0,0 @@
-insert into product (name, image, quantity, description, price) values
-("MacBook Pro Retina 13''", "images/macbookPro.jpg", 3, "Blah Blah Blah", 1000),
-("MacBook Air 13", "images/macbookAir.jpg", 3, "Blah Blah Blah", 700),
-("Iphone 8", "images/iphone8.jpg", 3, "Blah Blah Blah", 600),
-("MacBook AppleWatch 3G", "images/appleWatch3.jpg", 3, "Blah Blah Blah", 1000);
