c11dafb3d83190657c3b1df7fd2968d0e3e47af7
rustamov-bite <assasins_murza006@mail.ru>
2021-03-15 17:21:00 +0600

    added UserController

---
+++ src/main/java/com/shop/shop/controllers/UserController.java
@@ -0,0 +1,28 @@
+package com.shop.shop.controllers;
+
+import com.shop.shop.dto.UserDto;
+import com.shop.shop.exception.UserNotFoundException;
+import com.shop.shop.services.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+@AllArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/getUserById/{userId}")
+    public UserDto getUserById(@PathVariable("userId") Long userId){
+         var user = userService.getUserById(userId);
+         return UserDto.builder()
+                 .name(user.getName())
+                 .email(user.getEmail())
+                 .build();
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    private String userNotFound(UserNotFoundException uex) {
+        return uex.getMessage();
+    }
+}
