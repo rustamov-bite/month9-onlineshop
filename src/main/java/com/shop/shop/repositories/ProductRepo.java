426b7171ffd375bb94c55603f082171a48c390c1
rustamov-bite <assasins_murza006@mail.ru>
2021-03-18 14:22:04 +0600

    updated ProductRepo

--- src/main/java/com/shop/shop/repositories/ProductRepo.java
+++ src/main/java/com/shop/shop/repositories/ProductRepo.java
@@ -1,10 +1,15 @@
 package com.shop.shop.repositories;
 
 import com.shop.shop.models.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepo extends JpaRepository<Product, Long> {
-
+    Page<Product> findAllByNameContains(String name, Pageable pageable);
+    Page<Product> findAllByDescriptionContains(String description, Pageable pageable);
+    Page<Product> findAllByQuantityLessThanEqual(Integer quantity, Pageable pageable);
+    Page<Product> findAllByPriceLessThanEqual(Integer price, Pageable pageable);
 }
