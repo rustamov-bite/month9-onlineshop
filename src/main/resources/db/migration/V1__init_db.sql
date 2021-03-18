cbd3c64f656bb33cfb9cd79b4941bb2e3cc2b638
rustamov-bite <assasins_murza006@mail.ru>
2021-03-18 14:21:32 +0600

    updated pom.xml, application.properties, V1__init_db.sql

--- src/main/resources/db/migration/V1__init_db.sql
+++ src/main/resources/db/migration/V1__init_db.sql
@@ -1,7 +1,7 @@
 insert into categories (name) values ("MacBook"), ("iPhone"), ("Apple Watch");
 
 insert into products (name, image, quantity, description, price, category_id) values
-("MacBook Pro Retina 13''", "images/macbookPro.jpg", 3, "Blah Blah Blah", 1000, 0),
-("MacBook Air 13", "images/macbookAir.jpg", 3, "Blah Blah Blah", 700, 0),
-("Iphone 8", "images/iphone8.jpg", 3, "Blah Blah Blah", 600, 1),
-("AppleWatch 3G", "images/appleWatch3.jpg", 3, "Blah Blah Blah", 1000, 2);
+("MacBook Pro Retina 13''", "images/macbookPro.jpg", 3, "Blah Blah Blah", 1000, 1),
+("MacBook Air 13", "images/macbookAir.jpg", 3, "Blah Blah Blah", 700, 1),
+("Iphone 8", "images/iphone8.jpg", 3, "Blah Blah Blah", 600, 2),
+("AppleWatch 3G", "images/appleWatch3.jpg", 3, "Blah Blah Blah", 1000, 3);
